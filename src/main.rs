15d738465279da382e542a04311f07016ff7acd1
Will Farris <9190155+WillFarris@users.noreply.github.com>
2020-02-19 12:36:31 -0800

    Move to ggez from SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
-use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use ggez::*;
 
+struct critter {
+    x: u32,
+    y: u32,
+}
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem
-        .window("EcoSim", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-    
-    canvas.set_draw_color(Color::RGB(120, 200, 110));
-    canvas.clear();
-    canvas.present();
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    'running: loop {
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
-        update();
-        canvas.present();
+impl critter {
+    fn mov(&mut self) {
+        self.x += 1;
+        self.y += 1;
     }
 }
 
-fn update() {
+
+//120 200 110 <- color for background
+fn main() {
 
 }
