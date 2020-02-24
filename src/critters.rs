a0a53840fbd04f9fafb06ef1ba07b828c633046e
mahadd29 <mahadd29@users.noreply.github.com>
2020-02-24 01:35:12 -0800

    started implementing mating

--- src/critters.rs
+++ src/critters.rs
@@ -1,29 +1,29 @@
 pub mod critters {
-    use ggez::mint::Point2;
+    use crate::math::math::{anglebetween, distance};
     use ggez::graphics::Color;
+    use ggez::mint::Point2;
     use rand::{thread_rng, Rng};
-    use crate::math::math::{distance, anglebetween};
 
     pub const MAX_HUNGER: f32 = 500.0;
 
+    #[derive(Copy, Clone, Debug, PartialEq)]
     pub struct Prey {
-
         // Non-genetic traits, mostly for drawing and graphics
         pub position: Point2<f32>,
         pub color: Color,
         pub direction: Point2<f32>,
 
         // Genetic traits
-        pub size: f32,      // larger creatures can go longer without food
-        speed: f32,         // faster creatures consume energy more quickly
-        pub eyesight: f32,  // creatures with better eyesight can sense food further away
+        pub size: f32,     // larger creatures can go longer without food
+        speed: f32,        // faster creatures consume energy more quickly
+        pub eyesight: f32, // creatures with better eyesight can sense food further away
 
         pub max_hunger: f32, // depends on size
 
         // "Lifetime" traits
-        pub hunger: f32,        //decreases with movement, creature dies if it starves to death
-        pub is_dead: bool,      //dead flag, all dead creatures are removed each update() tick
-        pub wants_mate: bool,   //horny flag, signals whether or not the critter wants to mate
+        pub hunger: f32, //decreases with movement, creature dies if it starves to death
+        pub is_dead: bool, //dead flag, all dead creatures are removed each update() tick
+        pub wants_mate: bool, //horny flag, signals whether or not the critter wants to mate
     }
 
     impl Prey {
@@ -33,8 +33,14 @@ pub mod critters {
             let size: f32 = rng.gen_range(5.0, 10.0);
 
             Prey {
-                position: Point2 {x: rng.gen_range(0.0, 800.0), y: rng.gen_range(0.0, 600.0)},
-                direction: Point2 {x: angle.cos(), y: angle.sin()},
+                position: Point2 {
+                    x: rng.gen_range(0.0, 800.0),
+                    y: rng.gen_range(0.0, 600.0),
+                },
+                direction: Point2 {
+                    x: angle.cos(),
+                    y: angle.sin(),
+                },
                 color: Color::from_rgb(200, 50, 90),
 
                 speed: rng.gen_range(0.1, 0.5),
@@ -63,6 +69,9 @@ pub mod critters {
                 if self.hunger > MAX_HUNGER * 0.75 {
                     self.wants_mate = true;
                 }
+
+            //always horny for testing
+            //self.wants_mate = true;
             } else {
                 self.is_dead = true;
             }
@@ -92,7 +101,7 @@ pub mod critters {
             }
 
             if nearest_distance < self.size {
-                self.hunger +=  10.0 * target_food.sustenance;
+                self.hunger += 10.0 * target_food.sustenance;
                 if self.hunger > MAX_HUNGER {
                     self.hunger = MAX_HUNGER;
                 }
@@ -101,12 +110,23 @@ pub mod critters {
         }
 
         pub fn seek_mate(&mut self, target: &Prey) {
+            //let target: &Prey;
+            if !self.wants_mate {
+                return;
+            }
             let angle = anglebetween(&self.position, &target.position);
             self.direction.x = angle.cos();
             self.direction.y = angle.sin();
+
+            let dist = distance(&self.position, &target.position);
+            if dist < self.eyesight {
+                self.mate(target);
+            }
         }
 
-        pub fn mate(&self, _mate_with: &Prey) -> Prey {
+        pub fn mate(&mut self, _mate_with: &Prey) -> Prey {
+            self.wants_mate = false;
+            // _mate_with.wants_mate = false;
             Prey {
                 position: self.position,
                 color: Color::from_rgb(0, 0, 255),
@@ -117,13 +137,28 @@ pub mod critters {
                 max_hunger: 500.0,
                 hunger: 500.0,
                 is_dead: false,
-                wants_mate: false
+                wants_mate: false,
             }
         }
-    }
 
-    pub fn mate_prey(population: &mut Vec<Prey>) {
-        population.push(Prey::new());
+        //based on seek_food
+        pub fn mate_prey(&mut self, population: &mut Vec<Prey>) {
+            if self.wants_mate {
+                let mut partner = None;
+                let mut nearest_distance: f32 = std::f32::INFINITY;
+                let pop = population.clone();
+                for p in pop {
+                    let current_distance = distance(&self.position, &p.position) - p.size;
+                    if current_distance < nearest_distance && current_distance != 0.0 {
+                        partner = Some(p);
+                        nearest_distance = current_distance;
+                    }
+                }
+                if partner != None { //need to fix: never enters
+                    self.seek_mate(&partner.unwrap());
+                }
+            }
+        }
     }
 
     pub struct Food {
@@ -141,7 +176,10 @@ pub mod critters {
             let mut rng = thread_rng();
             let size = rng.gen_range(5.0, 15.0);
             Self {
-                position: Point2 {x: rng.gen_range(0.0, 800.0), y: rng.gen_range(0.0, 600.0)},
+                position: Point2 {
+                    x: rng.gen_range(0.0, 800.0),
+                    y: rng.gen_range(0.0, 600.0),
+                },
                 size: size,
                 color: Color::from_rgb(40, 90, 30),
                 sustenance: size * 2.0,
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+use ggez::event;
 use ggez::event::EventHandler;
 use ggez::graphics;
-use ggez::ContextBuilder;
-use ggez::graphics::{draw, Color, DrawParam, clear, present, Mesh, Rect};
-use ggez::event;
+use ggez::graphics::{clear, draw, present, Color, DrawParam, Mesh, Rect};
 use ggez::Context;
+use ggez::ContextBuilder;
 use ggez::GameResult;
 mod critters;
 mod math;
-use critters::critters::*;
 use crate::math::math::distance;
+use critters::critters::*;
 
 struct GameState {
     population: Vec<Prey>,
@@ -39,8 +39,8 @@ impl EventHandler for GameState {
         for i in &mut self.population {
             i.seek_food(&mut self.food);
             i.update();
+            i.mate_prey(&mut self.new_children);
         }
-
         self.population.append(&mut self.new_children);
         self.population.retain(|x| !x.is_dead);
         self.food.retain(|x| !x.consumed);
@@ -50,21 +50,61 @@ impl EventHandler for GameState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         clear(ctx, Color::from_rgb(120, 200, 110));
 
-
         for f in &self.food {
-            let mesh = Mesh::new_circle(ctx, graphics::DrawMode::fill(), f.position, f.size, 0.1, f.color)?;
+            let mesh = Mesh::new_circle(
+                ctx,
+                graphics::DrawMode::fill(),
+                f.position,
+                f.size,
+                0.1,
+                f.color,
+            )?;
             draw(ctx, &mesh, DrawParam::default())?;
         }
 
         for p in &self.population {
-            let eyesight_mesh = Mesh::new_circle(ctx, graphics::DrawMode::fill(), p.position, p.eyesight, 0.1, Color::from_rgba(255, 0,0, 50))?;
-            let body_mesh =     Mesh::new_circle(ctx, graphics::DrawMode::fill(), p.position, p.size, 0.1, p.color)?;
-            let hunger_bg =     Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), Rect {x: p.position.x - (3.0 * p.size), y: p.position.y - (3.0 * p.size), w: (6.0 * p.size), h: 10.0}, Color::from_rgb(100, 100, 100))?;
-            let hunger_bar =    Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), Rect {x: p.position.x - (3.0 * p.size), y: p.position.y - (3.0 * p.size), w: (6.0 * p.size) * (p.hunger / p.max_hunger), h: 10.0}, Color::from_rgb(255, 0, 0))?;
+            let eyesight_mesh = Mesh::new_circle(
+                ctx,
+                graphics::DrawMode::fill(),
+                p.position,
+                p.eyesight,
+                0.1,
+                Color::from_rgba(255, 0, 0, 50),
+            )?;
+            let body_mesh = Mesh::new_circle(
+                ctx,
+                graphics::DrawMode::fill(),
+                p.position,
+                p.size,
+                0.1,
+                p.color,
+            )?;
+            let hunger_bg = Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::fill(),
+                Rect {
+                    x: p.position.x - (3.0 * p.size),
+                    y: p.position.y - (3.0 * p.size),
+                    w: (6.0 * p.size),
+                    h: 10.0,
+                },
+                Color::from_rgb(100, 100, 100),
+            )?;
+            let hunger_bar = Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::fill(),
+                Rect {
+                    x: p.position.x - (3.0 * p.size),
+                    y: p.position.y - (3.0 * p.size),
+                    w: (6.0 * p.size) * (p.hunger / p.max_hunger),
+                    h: 10.0,
+                },
+                Color::from_rgb(255, 0, 0),
+            )?;
             draw(ctx, &eyesight_mesh, DrawParam::default())?;
-            draw(ctx, &body_mesh,     DrawParam::default())?;
-            draw(ctx, &hunger_bg,    DrawParam::default())?;
-            draw(ctx, &hunger_bar,    DrawParam::default())?;
+            draw(ctx, &body_mesh, DrawParam::default())?;
+            draw(ctx, &hunger_bg, DrawParam::default())?;
+            draw(ctx, &hunger_bar, DrawParam::default())?;
         }
 
         present(ctx)?;
@@ -73,10 +113,11 @@ impl EventHandler for GameState {
 }
 
 fn main() {
-    let (mut ctx, mut event_loop) = ContextBuilder::new("Ecosystem Simulation", "Will Farris, Maggie Haddon")
-        .window_setup(ggez::conf::WindowSetup::default().title("Ecosystem Simulation"))
-        .build()
-        .expect("Could not create ggez context!");
+    let (mut ctx, mut event_loop) =
+        ContextBuilder::new("Ecosystem Simulation", "Will Farris, Maggie Haddon")
+            .window_setup(ggez::conf::WindowSetup::default().title("Ecosystem Simulation"))
+            .build()
+            .expect("Could not create ggez context!");
 
     let mut game_state = GameState::new(&mut ctx);
 
